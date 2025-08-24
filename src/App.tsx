09c4f7a85a232fae2575852879cb6ef97227b41a
Emilio <128648742+3zavalam@users.noreply.github.com>
2025-08-23 23:48:52 -0600

    waitlist redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,17 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Waitlist from "./pages/Waitlist";
 
-const queryClient = new QueryClient();
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* / redirige directo a /waitlist */}
+        <Route path="/" element={<Navigate to="/waitlist" replace />} />
+        <Route path="/waitlist" element={<Waitlist />} />
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
-
-export default App;
+        {/* opcional: cualquier ruta desconocida manda a /waitlist */}
+        <Route path="*" element={<Navigate to="/waitlist" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
